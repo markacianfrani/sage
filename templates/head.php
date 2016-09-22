08bc4b45abf30ac32f4c68d680237bebd33140fb
markacianfrani <markacianf@gmail.com>
2016-09-22 19:47:47 -0400

    deploy scripts, mixins

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/at57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/at72.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/at114.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/at144.png" />
   <?php wp_head(); ?>
 </head>
